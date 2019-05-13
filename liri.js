c077220f25e48c51f68602826a421fa890a78a86
AlphaLy <lyalpha2401@gmail.com>
2019-05-13 11:14:10 -0500

    created the command line process.argv[i]

--- liri.js
+++ liri.js
@@ -6,3 +6,10 @@ var axios = require("axios");
 const keys = require("./keys.js");
 const Spotify = require('node-spotify-api');
 let spotify = new Spotify(keys.spotify);
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+//pull in required variables
+var spotifyTest = process.env.SPOTIFY_ID
+let command = process.argv[2]
+let searchTerm = process.argv[3]
